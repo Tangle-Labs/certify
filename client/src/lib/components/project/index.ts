14be161dd6f34350eb0b78f1a138ab38c5e3670f
coodos <info@coodos.co>
2022-12-08 20:06:58 +0530

    feat: dashboard layout

--- client/src/lib/components/project/index.ts
+++ client/src/lib/components/project/index.ts
@@ -1,1 +1,2 @@
 export { default as CardWithHeader } from "./CardWithHeader/CardWithHeader.svelte";
+export { default as Header } from "./Header/Header.svelte";
---
+++ client/src/routes/dashboard/+layout.svelte
@@ -0,0 +1,16 @@
+<style lang="scss">
+</style>
+
+<script lang="ts">
+	import { Header } from "$lib/components/project";
+</script>
+
+<div class="header">
+	<Header />
+</div>
+<div class="content-section">
+	<div class="sidenav" />
+	<div class="body-content">
+		<slot />
+	</div>
+</div>
