ce5d7970d81da30ec853b8a71f9a8981932fc956
coodos <info@coodos.co>
2022-12-12 16:15:04 +0530

    feat: added jwt utils

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,5 +1,5 @@
 export * from "./logger/logger";
-export * from "./passport/passport.util";
+export * from "./crypto/jwt.utils";
 export * from "./errors/errors.util";
 export * from "./misc/sanitize-object.util";
 export * from "./documentation/swagger";
