75cd2babd36255f93ce981dad95af5ef62f3247c
coodos <info@coodos.co>
2022-12-12 16:12:10 +0530

    refactor: fix middleware

--- server/@types/express/index.d.ts
+++ server/@types/express/index.d.ts
@@ -1,7 +1,14 @@
 import { UserModel } from "@/models/user.model";
+import { SessionModel } from "@/models/session.model";
 
 declare global {
 	namespace Express {
 		interface User extends UserModel {}
+		interface Session extends SessionModel {}
+
+		interface Request {
+			user?: User;
+			session?: Session;
+		}
 	}
 }
