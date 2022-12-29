b4a9a5341f3876d9417fe08773f3ccde42a1307c
coodos <info@coodos.co>
2022-12-29 12:13:33 +0530

    feat: `Credential` model

---
+++ server/src/models/credential.model.ts
@@ -0,0 +1,63 @@
+import {
+	DataTypes,
+	Model,
+	Sequelize,
+	InferAttributes,
+	InferCreationAttributes,
+	CreationOptional
+} from "sequelize";
+
+export type CredentialType = "license" | "certificate" | "ticket";
+
+export class CredentialModel extends Model<
+	InferAttributes<CredentialModel>,
+	InferCreationAttributes<CredentialModel>
+> {
+	declare id: CreationOptional<string>;
+	declare name: string;
+	declare referenceCode: string;
+	declare type: CredentialType;
+	declare duration: number;
+	declare customFields: Record<string, string>;
+}
+
+export const credentialModel = (db: Sequelize) => {
+	CredentialModel.init(
+		{
+			id: {
+				type: DataTypes.UUID,
+				primaryKey: true,
+				defaultValue: DataTypes.UUIDV4,
+				unique: true,
+				allowNull: false
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			referenceCode: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			type: {
+				type: DataTypes.ENUM,
+				values: ["ticket", "certificate", "license"],
+				allowNull: false
+			},
+			duration: {
+				type: DataTypes.INTEGER,
+				allowNull: false
+			},
+			customFields: {
+				type: DataTypes.JSON,
+				allowNull: false
+			}
+		},
+		{
+			sequelize: db,
+			modelName: "Credential"
+		}
+	);
+
+	return CredentialModel;
+};
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize";
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from "../config";
 import { userModel } from "./user.model";
 import { sessionModel } from "./session.model";
+import { credentialModel } from "./credential.model";
 
 const db = new Sequelize({
 	dialect: "postgres",
@@ -15,7 +16,9 @@ const db = new Sequelize({
 
 export const User = userModel(db);
 export const Session = sessionModel(db);
+export const Credential = credentialModel(db);
 
+// relations
 User.hasOne(Session, { foreignKey: "userId" });
 Session.belongsTo(User, { foreignKey: "userId" });
 
