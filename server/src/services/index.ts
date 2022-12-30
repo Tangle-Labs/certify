d72b619ef703f430dc413077d28e57c3ad63f891
coodos <info@coodos.co>
2022-12-30 16:06:46 +0530

    feat: `Application` model service

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,8 +1,9 @@
 import { ModelService } from "./model.service";
-import { Credential, Session, User } from "@/models";
+import { Application, Credential, Session, User } from "@/models";
 
 export const UsersService = new ModelService(User);
 export const CredentialsService = new ModelService(Credential);
 export const SessionsService = new ModelService(Session);
+export const ApplicationsService = new ModelService(Application);
 
 export * from "./identity.service";
