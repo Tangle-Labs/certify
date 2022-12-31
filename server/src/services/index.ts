677fbf5edf969fedb9bc79b80f285ff7216e792f
coodos <info@coodos.co>
2022-12-31 15:17:59 +0530

    feat: RoleService

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,9 +1,10 @@
 import { ModelService } from "./models/model.service";
-import { Application, Credential, Session, User } from "@/models";
+import { Application, Credential, Role, Session, User } from "@/models";
 
 export const UsersService = new ModelService(User);
 export const CredentialsService = new ModelService(Credential);
 export const SessionsService = new ModelService(Session);
 export const ApplicationsService = new ModelService(Application);
+export const RolesService = new ModelService(Role);
 
 export * from "./identity.service";
