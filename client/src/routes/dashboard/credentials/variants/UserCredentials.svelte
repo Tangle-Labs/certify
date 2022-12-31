f31042e6e5830f7e75f5f260779b23c65084ab5a
coodos <info@coodos.co>
2022-12-31 14:07:08 +0530

    feat: new application button on user side

--- client/src/routes/dashboard/credentials/variants/UserCredentials.svelte
+++ client/src/routes/dashboard/credentials/variants/UserCredentials.svelte
@@ -9,15 +9,25 @@
 
 		.sidebar {
 			width: 35%;
+			position: relative;
+
+			.new-cred {
+				width: 400px;
+				position: absolute;
+				top: -85px;
+				right: 0;
+			}
 		}
 	}
 </style>
 
 <script lang="ts">
 	import type { IApplication } from "$lib/types";
+	import { Button } from "$lib/components/ui";
 	import { apiClient } from "$lib/utils";
 	import CredentialsTable from "../credentials-components/CredentialsTable.svelte";
 	import ApplicationCard from "../../applications/application-components/ApplicationCard.svelte";
+	import { goto } from "$app/navigation";
 
 	let applications: IApplication[];
 	let selectedApplication: IApplication;
@@ -40,6 +50,13 @@
 		/>
 	</div>
 	<div class="sidebar">
+		<div class="new-cred">
+			<Button
+				label="Apply for Credential"
+				onClick={() => goto("/dashboard/credentials/new")}
+				size="large"
+			/>
+		</div>
 		<ApplicationCard bind:selected={selectedApplication} variant="user" />
 	</div>
 </div>
