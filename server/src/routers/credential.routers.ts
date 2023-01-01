cf1bca9daf4e5d5b4d9502f3b985188abfa4cc9b
coodos <info@coodos.co>
2023-01-01 14:13:17 +0530

    docs: organiztion and credential routers

--- server/src/routers/credential.routers.ts
+++ server/src/routers/credential.routers.ts
@@ -5,6 +5,75 @@ import { Router } from "express";
 
 const router = Router();
 
+/**
+ * @openapi
+ * "/api/credentials":
+ *   post:
+ *     tags:
+ *       - Credentials
+ *     summary: create a new Credential
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/CreateCredentialDto"
+ *     responses:
+ *       201:
+ *         description: Success, creates a new credential
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/CredentialResponse"
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Internal server error
+ *
+ *   get:
+ *     tags:
+ *       - Credentials
+ *     summary: get all the credentials
+ *     security:
+ *       - cookieAuth: []
+ *     responses:
+ *       200:
+ *         description: Success, returns array of all the credentials
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: "#/components/schemas/CredentialResposne"
+ *       401:
+ *         description: Unauthorized, happens when the user is not signed in
+ *       500:
+ *         description: Internal server error occurred
+ *
+ * "/api/credentials/{id}":
+ *   get:
+ *     tags:
+ *       - Credentials
+ *     summary: Edit a specific role
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *     responses:
+ *       202:
+ *         description: Success, updated the role
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/CredentialResponse"
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Internal Server Error
+ *
+ *
+ */
+
 router
 	.route("/")
 	.post(
--- server/src/routers/organization.routers.ts
+++ server/src/routers/organization.routers.ts
@@ -5,6 +5,50 @@ import { Router } from "express";
 
 const router = Router();
 
+/**
+ * @openapi
+ * "/api/organization":
+ *   patch:
+ *     tags:
+ *       - Organization
+ *     summary: Update the Organization's details
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/UpdateOrganizationDto"
+ *     responses:
+ *       201:
+ *         description: Success, updated the organization
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/OrganizationResponse"
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Internal server error
+ *
+ *   get:
+ *     tags:
+ *       - Organization
+ *     summary: get details of the organization
+ *     security:
+ *       - cookieAuth: []
+ *     responses:
+ *       200:
+ *         description: Success, returns the organization details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/OrganizationResponse"
+ *       401:
+ *         description: Unauthorized, happens when the user is not signed in
+ *       500:
+ *         description: Internal server error occurred
+ *
+ */
+
 router
 	.route("/")
 	.get(isAuthenticated, isStaff, hasPermission("manageOrganization"), getOrganizationProfile)
