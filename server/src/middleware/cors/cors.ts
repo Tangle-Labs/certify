d743d9adf6a2cead5b2ad72c41c2a2d67674e6df
Coodos <info@coodos.co>
2023-08-11 19:39:58 +0530

    chore: upgrades

--- server/src/middleware/cors/cors.ts
+++ server/src/middleware/cors/cors.ts
@@ -3,7 +3,10 @@ import { ALLOWED_ORIGINS } from "@/config";
 const origin = ALLOWED_ORIGINS.split(",").map((e) => e.trim());
 
 export const corsConfig = {
-	origin,
+	origin: function (originReceived, cb) {
+		if (origin.includes(originReceived)) return cb(null, true);
+		return cb("Cors not allowed");
+	},
 	credentials: true,
 	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 	allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "token", "Authorization"]
