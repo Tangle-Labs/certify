801c42571f50e119a452d83eac52627abc0ab89b
Coodos <info@coodos.co>
2023-08-14 15:36:40 +0530

    feat: hookup router

--- server/src/routers/root.router.ts
+++ server/src/routers/root.router.ts
@@ -1,8 +1,10 @@
-import { getInfo } from "@/controllers";
+import { getDashboardStats, getInfo } from "@/controllers";
+import { isAuthenticated } from "@/middleware";
 import { Router } from "express";
 
 const router = Router();
 
 router.route("/info").get(getInfo);
+router.route("/stats").get(isAuthenticated, getDashboardStats);
 
 export default router;
