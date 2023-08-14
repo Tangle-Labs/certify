767db920af840c652af23046845a199ea57b4f9d
Coodos <info@coodos.co>
2023-08-14 15:35:55 +0530

    feat: stats controller

--- server/src/controllers/root.controller.ts
+++ server/src/controllers/root.controller.ts
@@ -1,3 +1,4 @@
+import { ApplicationsService } from "@/services";
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 
@@ -6,4 +7,15 @@ export const getInfo = asyncHandler(async (req: Request, res: Response) => {
 		version: "v0.1.0",
 		name: "Certify"
 	});
+});
+
+export const getDashboardStats = asyncHandler(async (req: Request, res: Response) => {
+	const applicationStats = {
+		approved: 0,
+		pending: 0,
+		rejected: 0
+	};
+	const applications = await ApplicationsService.findMany({ userId: req.user.id });
+	applications.forEach((a) => applicationStats[a.status]++);
+	res.json(applicationStats);
 });
